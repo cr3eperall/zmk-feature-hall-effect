965dc723b3c9cea4be35f0ab59d4dcd5d32d1813
cr3eperall <davide.michelotti04@gmail.com>
2025-04-15 15:45:44 +0000

    add input-processor-matrix-offset, begin implementing adj. actuation

---
+++ src/input_processors/input_processor_matrix_offset.c
@@ -0,0 +1,34 @@
+#include <drivers/input_processor.h>
+#include <stdlib.h>
+#include <zephyr/device.h>
+#include <zephyr/logging/log.h>
+
+#include "input-event-codes.h"
+
+LOG_MODULE_REGISTER(input_processor_matrix_offset,
+                    CONFIG_LOG_MAX_LEVEL);  // TODO change log level
+
+#define DT_DRV_COMPAT he_input_processor_matrix_offset
+
+static int matrix_offset_handle_event(const struct device *dev,
+                               struct input_event *event, uint32_t x,
+                               uint32_t y,
+                               struct zmk_input_processor_state *state) {
+    if (event->type != INPUT_EV_HE) return ZMK_INPUT_PROC_CONTINUE;
+    uint8_t row=INV_INPUT_HE_ROW(event->code);
+    uint8_t col=INV_INPUT_HE_COL(event->code);
+    event->code=INPUT_HE_RC(row+y,col+x);
+    return ZMK_INPUT_PROC_CONTINUE;
+}
+
+static struct zmk_input_processor_driver_api processor_api = {
+    .handle_event = matrix_offset_handle_event,
+};
+
+#define MAT_OFFSET_INIT(n)                                                        \                                                          \
+    DEVICE_DT_INST_DEFINE(n, NULL, NULL,                              \
+                          NULL,                     \
+                          NULL, POST_KERNEL,      \
+                          CONFIG_INPUT_INIT_PRIORITY, &processor_api);
+
+DT_INST_FOREACH_STATUS_OKAY(MAT_OFFSET_INIT)
